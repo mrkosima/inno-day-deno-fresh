f97147413efbd498f1d604ef37dbd04dce867077
Kanstantsin Klimashevich <kklimashevich@xebia.com>
2022-10-27 12:02:47 +0200

    Add game ala wordle

--- routes/game.tsx
+++ routes/game.tsx
@@ -1,26 +1,48 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
+import type { FunctionComponent } from 'preact';
 
 type Data = {
   answer: string;
-  result: boolean | undefined;
+  length: number;
+  success: boolean | undefined;
+  matches: ("none" | "present" | "match")[];
+  cheat: boolean | undefined;
 }
 
-const GAME_ANSWER = Deno.env.get('GAME_ANSWER');
+const GAME_ANSWER = (Deno.env.get('GAME_ANSWER') || '').toLowerCase();
 
 export const handler: Handlers<Data> = {
   GET(req, ctx) {
     const url = new URL(req.url);
-    const answer = url.searchParams.get("answer") || "";
-    return ctx.render({ answer, result: answer ? answer === GAME_ANSWER : undefined })
+    const originAnswer = url.searchParams.get("answer");
+    const answer = (originAnswer || "").trim().toLowerCase();
+    let matches: Data['matches'] = [];
+    if (answer.length === GAME_ANSWER.length) {
+      matches = answer.split('').map((c, i) => {
+        if (GAME_ANSWER[i] === c) {
+          return 'match';
+        }
+        if (GAME_ANSWER.indexOf(c) >= 0) {
+          return 'present';
+        }
+        return 'none';
+      })
+    }
+
+    return ctx.render({
+      length: GAME_ANSWER.length,
+      answer,
+      matches,
+      success: answer ? answer === GAME_ANSWER : undefined,
+      cheat: answer ? answer.length !== GAME_ANSWER.length : undefined,
+    })
   },
 };
 
-  export default function Game({ data }: PageProps<Data>) {
-    const { result, answer } = data;
-return (
+const Wrapper: FunctionComponent<{ answer: string, length: number }> = ({ children, answer, length }) => (
   <div className="container">
     <div className="font-mono max-w-sm mx-auto">
-      <h1 className="text-2xl my-4">Game: type in my name</h1>
+      <h1 className="text-2xl my-4">Game: guess the word of the day which has <span className="text-red-900">{length}</span> characters</h1>
       <form className="mb-4">
         <input
           type="text"
@@ -29,20 +51,47 @@ return (
           value={answer} />
         <button
           type="submit"
-          className="text-gray-700 bg-gray-100 hover:bg-gray-300 focus:outline-none rounded-sm font-medium text-sm w-full px-5 py-2.5 text-center"
+          className="text-gray-100 rounded bg-green-500 hover:bg-green-400 focus:outline-none rounded-sm font-medium text-sm w-full px-5 py-2.5 text-center"
         >Submit</button>
       </form>
-      {result !== undefined && (
-        result ?
-          (
-            <div className="bg-green-100 text-green-900 text-center p-8">You're a great person!</div>
-          )
-          :
-          (
-            <div className="bg-red-100 text-red-900 text-center p-8">Shame on you!</div>
-          )
-      )}
+      {children}
     </div>
-  </div >
-);
+  </div>
+)
+
+export default function Game({ data }: PageProps<Data>) {
+  const { success, cheat, answer, length, matches } = data;
+  if (cheat) {
+    return (
+      <Wrapper answer={answer} length={length}>
+        <div className="bg-red-100 text-red-900 text-center p-8 rounded">Don't cheat. The word should be {length} chars</div>
+      </Wrapper>
+    )
+  }
+  if (success) {
+    return (
+      <Wrapper answer={answer} length={length}>
+        <div className="bg-green-100 text-green-900 text-center p-8 rounded">Congrats, <span className="font-bold">"{answer}"</span> is the word I riddled!</div>
+      </Wrapper>
+    )
+  }
+  return (
+    <Wrapper answer={answer} length={length}>
+      {matches?.length && (
+        <div className="flex gap-1 mt-12 justify-center">
+          {matches.map((match, i) => {
+            let bg = "bg-gray-500";
+            if (match === 'match') {
+              bg = "bg-green-500"
+            }
+            if (match === 'present') {
+              bg = "bg-yellow-500"
+            }
+            return <div className={`p-4 ${bg} rounded text-white`}>{answer[i]}</div>
+          })}
+        </div>
+      )}
+
+    </Wrapper>
+  )
 }
